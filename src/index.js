607da5a16308212fa358bc7bc56be159b04a96eb
playsoksana <playsoksana@gmail.com>
2021-05-30 22:52:41 +0300

    спинер работает

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import maktupList from './js/templates/maktupList.hbs'
 import ApiService from './js/apiService.js';
 import scrollEnd from './js/scroll.js'
 // == 
-//const basicLightbox = require('basiclightbox');
-//import * as basicLightbox from 'basicLightbox';
 const basicLightbox = require('basiclightbox');
+// import * as basicLightbox from 'basicLightbox'; 
+// ==
+import {Spinner} from 'spin.js';
 
-
-// ==S
+// ==
 const refs = Refs();
 const apiService = new ApiService;
 // ==
@@ -54,161 +54,30 @@ function clearCardsOnDom() {
     refs.list.innerHTML = '';
 };
 
-// refs.list.addEventListener('click', openModal);
-// function openModal(ev) {
-//     console.log(ev.target.getAttribute('data'));
-//     const linkImg = ev.target.getAttribute('data');
+// ==============
 
 
-//     const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <img class='img' src=${linkImg} alt="image">
-//     </div>
-// `);
-//     instance.show();
-
-//    }
-
- 
-refs.list.onclick = (ev) => {
-    if (!ev.target.getAttribute('data')) {
-        return;
-    }
-    const linkImg = ev.target.getAttribute('data');
-    const instance = basicLightbox.create(`
-     <div class="modal" >
-        <img class='img' src=${linkImg} alt="image" allowfullscreen>
-     </div>
- `);
-    instance.show();
-    document.querySelector('.modal').onclick = (ev) => {
-     instance.close();
-}
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 2, // The length of each line
+  width: 3, // The line thickness
+  radius: 5, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#ffffff', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '-7px', // Top position relative to parent
+  left: '7%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
 };
 
-
-
-// ====================
-
-
-// document.querySelector('button.image').onclick = () => {
-
-// 	basicLightbox.create(`
-// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
-// 	`).show()
-
-// }
-
-// document.querySelector('button.iframe').onclick = () => {
-
-// 	basicLightbox.create(`
-// 		<iframe width="560" height="315" src="https://www.youtube.com/embed/Scxs7L0vhZ4" frameborder="0" allowfullscreen></iframe>
-// 	`).show()
-
-// }
-
-// document.querySelector('button.video').onclick = () => {
-
-// 	basicLightbox.create(`
-// 		<video controls data-id="2">
-// 			<source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
-// 		</video>
-// 	`).show()
-
-// }
-
-// document.querySelector('button.html').onclick = () => {
-
-// 	basicLightbox.create(`
-// 		<h1>HTML</h1>
-// 		<p>HTML inside a lightbox.</p>
-// 	`).show()
-
-// }
-
-// document.querySelector('button.callbacks').onclick = (e) => {
-
-// 	const html = `
-// 		<h1>Callbacks</h1>
-// 		<p>Take a look at the console of your browser.<br>This lightbox will close automaticly to demonstrate the close-callback.</p>
-// 	`
-
-// 	const instance = basicLightbox.create(html, {
-// 		onShow: (instance) => console.log('onShow', instance),
-// 		onClose: (instance) => console.log('onClose', instance)
-// 	})
-
-// 	instance.show((instance) => console.log('finished show()', instance))
-
-// 	setTimeout(() => {
-// 		instance.close((instance) => console.log('finished close()', instance))
-// 	}, 3000)
-
-// }
-
-// document.querySelector('button.blocked').onclick = () => {
-
-// 	const html = `
-// 		<h1>Blocked</h1>
-// 		<p>It's not possible to close this lightbox,<br>because the onClose function returns false.</p>
-// 	`
-
-// 	basicLightbox.create(html, {
-// 		onClose: () => false
-// 	}).show()
-
-// }
-
-
-// document.querySelector('button.notClosable').onclick = () => {
-
-// 	const html = `
-// 		<h1>Not closable</h1>
-// 		<p>It's not possible to close this lightbox with a click.</p>
-// 	`
-
-// 	basicLightbox.create(html, {
-// 		closable: false
-// 	}).show()
-
-// }
-
-// document.querySelector('button.modify').onclick = () => {
-
-// 	const instance = basicLightbox.create(`
-// 		<h1>Modify HTML</h1>
-// 		<p>You can modify the HTML of the lightbox using JS.</p>
-// 	`)
-
-// 	instance.element().insertAdjacentHTML('afterbegin', '<p>Before placeholder</p>')
-// 	instance.element().insertAdjacentHTML('beforeend', '<p>After placeholder</p>')
-
-// 	instance.show()
-
-// }
-
-// document.querySelector('button.classNames').onclick = () => {
-
-// 	const html = `
-// 		<h1>Custom classNames</h1>
-// 		<p>You can add custom classNames to the lightbox element.</p>
-// 	`
-
-// 	basicLightbox.create(html, {
-// 		className: 'classNames one two three'
-// 	}).show()
-
-// }
-
-// const stack = () => {
-
-// 	const html = `<button>Open another lightbox</button>`
-// 	const instance = basicLightbox.create(html)
-
-// 	instance.element().querySelector('button').onclick = stack
-// 	instance.show()
-
-// }
-
-// document.querySelector('button.stack').onclick = stack
-
+var target = document.getElementById('spinner');
+var spinner = new Spinner(opts).spin(target);
+spinner.spin(target);
